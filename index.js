44fdb068ea04c73c3b6d96b21bba75e3bc5a89df
pychangeric <pychangeric@gmail.com>
2023-03-13 17:04:05 +0300

    compeleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+let companyName = 'Scuber';
+
+ companyName == 'Scuber';
+
+let mostProfitableNeighborhood='Chelsea';
+
+mostProfitableNeighborhood == 'Chelsea';
+
+let companyCeo='Susan Smith';
+
+companyCeo == 'Susan Smith';
+
+
